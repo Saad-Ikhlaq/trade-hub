37699940b206cb135ed78ef9c41e272b7ec89983
Saad-Ikhlaq <mohammad.saad7991@gmail.com>
2024-05-31 10:23:24 +0500

    Nodemailer bug fixes

--- backend/utils/sendEmails.js
+++ backend/utils/sendEmails.js
@@ -1,30 +1,29 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-export const sendEmail = (name, email, verificationString) =>{
-
+export const sendEmail = (name, email, verificationString) => {
   var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'bcsf20a019@pucit.edu.pk',
-      pass: '!be9317f9b8'
-    }
+      user: "mohammad.saad7991@gmail.com",
+      pass: "fhwa qksw lbnb qifb",
+    },
   });
-  
+
   var mailOptions = {
-    from: 'bcsf20a019@pucit.edu.pk',
+    from: "mohammad.saad7991@gmail.com",
     to: email,
-    subject: 'Sending Email using Node.js',
+    subject: "Sending Email using Node.js",
     text: `Hey ${name} please verify your email address: 
-    Click on the below link to verify your account: http://localhost:3000/verify-email/${verificationString}`
+    Click on the below link to verify your account: http://localhost:3000/verify-email/${verificationString}`,
   };
-  
-  transporter.sendMail(mailOptions, function(error, info){
+
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log("Email sent: " + info.response);
     }
   });
-}
+};
 
-export default sendEmail
+export default sendEmail;
